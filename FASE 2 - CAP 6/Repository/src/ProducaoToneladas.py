f862f4e53b84132dd17a5ea71b20022c8b1e2081
rm563003 <rm563003@fiap.com.br>
2025-04-25 00:05:39 -0300

    Add files via upload

---
+++ FASE 2 - CAP 6/Repository/src/ProducaoToneladas.py
@@ -0,0 +1,41 @@
+# FatimaCandal_RM563003_fase2_cap6
+
+# Um sistema para gerenciar dados de produção agrícola, como como quantidade colhida,
+# perdas, área plantada, área colhida, produção e rendimento médio.
+
+# Levantamento Sistemático da Produção Agrícola - PE
+# Estimativas para a safra de cana anual de Pernambuco
+
+# Tabela 6588 - Série histórica da estimativa anual da área plantada, área colhida, produção e rendimento
+# médio dos produtos das lavouras
+# Unidade da Federação - Pernambuco
+# Produto das lavouras - 11 Cana-de-açúcar
+
+# Fonte: IBGE - Levantamento Sistemático da Produção Agrícola
+# Os dados são uma atualização mensal da estimativa para a safra COMPLETA, anual.
+# Não é uma estimativa para a produção mensal.
+
+
+#install package matplotlib
+#install package pandas
+
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Load the Excel file
+df = pd.read_excel("C:/FIAP/tabela6588.xlsx")
+
+print(df.head())
+
+
+# Generate a line plot for 'Produção (Toneladas)' over time
+plt.figure(figsize=(10, 6))
+plt.plot(df['Mês'], df['Produção (Toneladas)'], marker='o', color='green')
+plt.title('Produção (Toneladas) ao longo do tempo')
+plt.xlabel('Mês')
+plt.xticks(rotation=75)
+plt.ylabel('Produção (Toneladas)')
+plt.grid(True)
+plt.tight_layout()
+plt.savefig('producao.png')
+plt.show()
